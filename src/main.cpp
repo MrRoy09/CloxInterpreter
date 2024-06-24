ab49a660cfe369d5c692dd77bb90f16c0d0d6881
MrRoy09 <royketan09@gmail.com>
2024-06-24 12:26:53 +0530

    minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -5,38 +5,42 @@
 #include "debug.h"
 #include "vm.h"
 #include "compiler.h"
-#include <fstream> 
+#include <fstream>
 #include <sstream>
 
-
-static void repl(VM *vm) {
-    while (1) {
+static void repl(VM *vm)
+{
+    while (1)
+    {
         std::string input;
         std::cout << "> ";
-        std::getline(std::cin,input);
-        if (input == "exit") {
+        std::getline(std::cin, input);
+        if (input == "exit")
+        {
             break;
         }
         InterpretResult result = vm->interpret(input);
-        if (result != INTERPRET_OK) {
+        if (result != INTERPRET_OK)
+        {
             exit(10);
         }
     }
 }
 
-int main(int argc , const char*argv[])
+int main(int argc, const char *argv[])
 {
     VM vm;
-    if (argc == 2) {
+    if (argc == 2)
+    {
         std::ifstream code(argv[1]);
         std::stringstream buffer;
         buffer << code.rdbuf();
         std::string code_string = buffer.str();
         vm.interpret(code_string);
     }
-    else { 
-        repl(&vm); 
+    else
+    {
+        std::cout << "Missing argument filepath" << "\n";
     }
     return 0;
 }
-
