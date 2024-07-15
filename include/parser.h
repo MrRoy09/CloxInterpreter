44f7aa8add974b75701da2e3a945fd112f1cb797
MrRoy09 <royketan09@gmail.com>
2024-07-15 20:11:16 +0530

    minor fix

--- include/parser.h
+++ include/parser.h
@@ -1,47 +1,61 @@
 #pragma once
-class Scanner {
+class Scanner
+{
 public:
-	const char* start;
-	const char* current;
+	const char *start;
+	const char *current;
 	int line;
-	bool isAtEnd() {
+	bool isAtEnd()
+	{
 		return *current == '\0';
 	}
 
-	bool isDigit(char c) {
-		if (c >= '0' && c <= '9') {
+	bool isDigit(char c)
+	{
+		if (c >= '0' && c <= '9')
+		{
 			return 1;
 		}
 		return 0;
 	}
 
-	bool isAlpha(char c) {
+	bool isAlpha(char c)
+	{
 		return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_');
 	}
 
-	char advance() {
+	char advance()
+	{
 		current++;
 		return current[-1];
 	}
 
-	bool match(char expected) {
-		if (isAtEnd()) return false;
-		if (*current != expected) return false;
+	bool match(char expected)
+	{
+		if (isAtEnd())
+			return false;
+		if (*current != expected)
+			return false;
 		current++;
 		return true;
 	}
 
-	void consumeWhitespace() {
-		if (*current == ' ' || *current == '\r' || *current == '\t') {
+	void consumeWhitespace()
+	{
+		if (*current == ' ' || *current == '\r' || *current == '\t')
+		{
 			while (*current == ' ' || *current == '\r' || *current == '\t')
 				advance();
 		}
 		start = current;
 	}
 
-	void consumeEmptyLine() {
-		if (*current =='\n') {
-			while (*current == '\n') {
+	void consumeEmptyLine()
+	{
+		if (*current == '\n')
+		{
+			while (*current == '\n')
+			{
 				advance();
 				consumeWhitespace();
 			}
@@ -49,38 +63,58 @@ public:
 		start = current;
 	}
 
-	Token scanToken() {
+	Token scanToken()
+	{
 		start = current;
-		// Handle for whitespaces 
-		if (isAtEnd()) return makeToken(TOKEN_EOF);
+		// Handle for whitespaces
 		consumeWhitespace();
-		if (*current == '\n') {
+		if (*current == '\n')
+		{
 			line++;
 			advance();
 			consumeWhitespace();
 			consumeEmptyLine();
 		}
 
+		if (isAtEnd())
+			return makeToken(TOKEN_EOF);
+
 		// Handle for comments
-		if (*current == '/' && *current + 1 == '/') {
-			while (*current != '\n' && !isAtEnd()) advance();
+		if (*current == '/' && *current + 1 == '/')
+		{
+			while (*current != '\n' && !isAtEnd())
+				advance();
 		}
 
 		char c = advance();
-		if (isDigit(c)) return number();
-		if (isAlpha(c)) return identifier();
-		switch (c) {
-		case '(': return makeToken(TOKEN_LEFT_PAREN);
-		case ')': return makeToken(TOKEN_RIGHT_PAREN);
-		case '{': return makeToken(TOKEN_LEFT_BRACE);
-		case '}': return makeToken(TOKEN_RIGHT_BRACE);
-		case ';': return makeToken(TOKEN_SEMICOLON);
-		case ',': return makeToken(TOKEN_COMMA);
-		case '.': return makeToken(TOKEN_DOT);
-		case '-': return makeToken(TOKEN_MINUS);
-		case '+': return makeToken(TOKEN_PLUS);
-		case '/': return makeToken(TOKEN_SLASH);
-		case '*': return makeToken(TOKEN_STAR);
+		if (isDigit(c))
+			return number();
+		if (isAlpha(c))
+			return identifier();
+		switch (c)
+		{
+		case '(':
+			return makeToken(TOKEN_LEFT_PAREN);
+		case ')':
+			return makeToken(TOKEN_RIGHT_PAREN);
+		case '{':
+			return makeToken(TOKEN_LEFT_BRACE);
+		case '}':
+			return makeToken(TOKEN_RIGHT_BRACE);
+		case ';':
+			return makeToken(TOKEN_SEMICOLON);
+		case ',':
+			return makeToken(TOKEN_COMMA);
+		case '.':
+			return makeToken(TOKEN_DOT);
+		case '-':
+			return makeToken(TOKEN_MINUS);
+		case '+':
+			return makeToken(TOKEN_PLUS);
+		case '/':
+			return makeToken(TOKEN_SLASH);
+		case '*':
+			return makeToken(TOKEN_STAR);
 
 		case '!':
 			return makeToken(
@@ -97,96 +131,131 @@ public:
 
 		case '"':
 			return String();
-
 		}
 		return errorToken("Unexpected character.");
 	}
 
-	Token makeToken(TokenType type) {
+	Token makeToken(TokenType type)
+	{
 		Token token = Token(type, start, (int)(current - start), line);
-		//std::cout << token.type << "\n";
+		// std::cout << token.type << "\n";
 		return token;
 	}
 
-	Token errorToken(const char* message) {
+	Token errorToken(const char *message)
+	{
 		Token token = Token(TOKEN_ERROR, start, (int)(current - start), line);
 		return token;
 	}
 
-	Token String() {
-		while (*current != '"' && !isAtEnd()) {
-			if (*current == '\n') line++;
+	Token String()
+	{
+		while (*current != '"' && !isAtEnd())
+		{
+			if (*current == '\n')
+				line++;
 			advance();
 		}
-		if (isAtEnd()) {
+		if (isAtEnd())
+		{
 			return errorToken("Unterminated String");
 		}
 		advance();
 		return makeToken(TOKEN_STRING);
 	}
 
-	Token number() {
-		while (isDigit(*current) && !isAtEnd()) {
+	Token number()
+	{
+		while (isDigit(*current) && !isAtEnd())
+		{
 			advance();
 		}
-		if (*current == '.') {
+		if (*current == '.')
+		{
 			advance();
-			while (isDigit(*current)) {
+			while (isDigit(*current))
+			{
 				advance();
 			}
 		}
 		return makeToken(TOKEN_NUMBER);
 	}
 
-	Token identifier() {
-		while ((isAlpha(*current) || isDigit(*current)) && !isAtEnd()) {
+	Token identifier()
+	{
+		while ((isAlpha(*current) || isDigit(*current)) && !isAtEnd())
+		{
 			advance();
 		}
 		return makeToken(TokenTypeIdentifier());
 	}
 
-	TokenType TokenTypeIdentifier() {
+	TokenType TokenTypeIdentifier()
+	{
 
-		switch (*start) {
-		case 'a': return checkKeyword(1, 2, "nd", TOKEN_AND);
-		case 'c': return checkKeyword(1, 4, "lass", TOKEN_CLASS);
-		case 'e': return checkKeyword(1, 3, "lse", TOKEN_ELSE);
-		case 'f': {
-			if (current - start > 1) {
-				switch (start[1]) {
-				case 'a': return checkKeyword(2, 3, "lse", TOKEN_FALSE);
-				case 'u': return checkKeyword(2, 1, "n", TOKEN_FUN);
-				case 'o': return checkKeyword(2, 1, "r", TOKEN_FOR);
-				}
-
-			}
-			break;
-		}
-		case 'i': return checkKeyword(1, 1, "f", TOKEN_IF);
-		case 'n': return checkKeyword(1, 2, "il", TOKEN_NIL);
-		case 'o': return checkKeyword(1, 1, "r", TOKEN_OR);
-		case 'p': return checkKeyword(1, 4, "rint", TOKEN_PRINT);
-		case 'r': return checkKeyword(1, 5, "eturn", TOKEN_RETURN);
-		case 's': return checkKeyword(1, 4, "uper", TOKEN_SUPER);
-		case 't': {
-			if (current - start > 1) {
-				switch (start[1]) {
-				case 'h': return checkKeyword(2, 2, "is", TOKEN_THIS);
-				case 'r': return checkKeyword(2, 2, "ue", TOKEN_TRUE);
+		switch (*start)
+		{
+		case 'a':
+			return checkKeyword(1, 2, "nd", TOKEN_AND);
+		case 'c':
+			return checkKeyword(1, 4, "lass", TOKEN_CLASS);
+		case 'e':
+			return checkKeyword(1, 3, "lse", TOKEN_ELSE);
+		case 'f':
+		{
+			if (current - start > 1)
+			{
+				switch (start[1])
+				{
+				case 'a':
+					return checkKeyword(2, 3, "lse", TOKEN_FALSE);
+				case 'u':
+					return checkKeyword(2, 1, "n", TOKEN_FUN);
+				case 'o':
+					return checkKeyword(2, 1, "r", TOKEN_FOR);
 				}
 			}
 			break;
 		}
-		case 'v': return checkKeyword(1, 2, "ar", TOKEN_VAR);
-		case 'w': return checkKeyword(1, 4, "hile", TOKEN_WHILE);
-
+		case 'i':
+			return checkKeyword(1, 1, "f", TOKEN_IF);
+		case 'n':
+			return checkKeyword(1, 2, "il", TOKEN_NIL);
+		case 'o':
+			return checkKeyword(1, 1, "r", TOKEN_OR);
+		case 'p':
+			return checkKeyword(1, 4, "rint", TOKEN_PRINT);
+		case 'r':
+			return checkKeyword(1, 5, "eturn", TOKEN_RETURN);
+		case 's':
+			return checkKeyword(1, 4, "uper", TOKEN_SUPER);
+		case 't':
+		{
+			if (current - start > 1)
+			{
+				switch (start[1])
+				{
+				case 'h':
+					return checkKeyword(2, 2, "is", TOKEN_THIS);
+				case 'r':
+					return checkKeyword(2, 2, "ue", TOKEN_TRUE);
+				}
+			}
+			break;
+		}
+		case 'v':
+			return checkKeyword(1, 2, "ar", TOKEN_VAR);
+		case 'w':
+			return checkKeyword(1, 4, "hile", TOKEN_WHILE);
 		}
 		return TOKEN_IDENTIFIER;
 	}
 
-	TokenType checkKeyword(int startC, int length, const char* check_str, TokenType checkToken) {
+	TokenType checkKeyword(int startC, int length, const char *check_str, TokenType checkToken)
+	{
 		if (current - start == startC + length &&
-			memcmp(start + startC, check_str, length) == 0) {
+			memcmp(start + startC, check_str, length) == 0)
+		{
 			return checkToken;
 		}
 
@@ -194,48 +263,56 @@ public:
 	}
 };
 
-class Parser {
+class Parser
+{
 public:
 	Token current;
 	Token previous;
-	Scanner* scanner;
+	Scanner *scanner;
 	bool had_error = 0;
-	const char* source;
+	const char *source;
 
-	Parser(const char* src, Scanner* scanner) : source(src), current(TOKEN_NONE, 0, 0, 0), previous(TOKEN_NONE, 0, 0, 0) {
+	Parser(const char *src, Scanner *scanner) : source(src), current(TOKEN_NONE, 0, 0, 0), previous(TOKEN_NONE, 0, 0, 0)
+	{
 		this->scanner = scanner;
 	}
 
-	void advance() {
+	void advance()
+	{
 		previous = current;
 		current = scanner->scanToken();
-		//std::cout << previous.type <<" " << current.type<<"\n";
+		// std::cout << previous.type <<" " << current.type<<"\n";
 	}
 
-	void consume(TokenType type, const char* message) {
+	void consume(TokenType type, const char *message)
+	{
 
-		if (current.type == type) {
+		if (current.type == type)
+		{
 			advance();
 			return;
 		}
-		else {
-			//std::cout << "Current token type is " << current.type << " Expected type is " << type << "\n";
+		else
+		{
+			// std::cout << "Current token type is " << current.type << " Expected type is " << type << "\n";
 			errorAtCurrent(message);
 		}
 	}
 
-	void errorAt(Token* token, const char* message) {
+	void errorAt(Token *token, const char *message)
+	{
 		std::cerr << "Error at " << token->line << "\n";
 		std::cerr << message << "\n";
 		had_error = 1;
 	}
 
-	void errorAtCurrent(const char* message) {
+	void errorAtCurrent(const char *message)
+	{
 		errorAt(&current, message);
 	}
 
-	void error(const char* message) {
+	void error(const char *message)
+	{
 		errorAt(&previous, message);
 	}
-
 };
